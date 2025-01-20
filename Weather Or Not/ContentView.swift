d182ea6d9eb1d87fb177da619ed630e586b55524
Luke Bresler <luke@realpixel.co.za>
2025-01-20 10:53:44 +0200

    first commit

--- Weather Or Not/ContentView.swift
+++ Weather Or Not/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Let there be light!")
         }
         .padding()
     }
